cde4cc61cc7e4e098bc91fcdbe6389b5cff7567b
Nicolò Casagrande <nicolo.casagrande7@gmail.com>
2022-11-24 09:18:42 +0100

    fine

---
+++ src/components/AppHero.vue
@@ -0,0 +1,26 @@
+<script>
+  export default{
+    data(){
+      return {
+        title: 'Titolo hero', 
+        hero: 'Questa è una hero di esempio con del testo casuale'
+      }
+    }
+  }
+</script>
+
+<template>
+    <div class="container">
+        <h2>{{ title }}</h2>
+        <p>{{ hero }}</p>
+    </div>
+</template>
+
+<style scoped>
+h2{
+  color: black;
+}
+.container{
+    margin: auto;
+}
+</style>
