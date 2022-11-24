01fff72e93b6421e43f1f8bb6d3055e248e090a1
Nicolò Casagrande <nicolo.casagrande7@gmail.com>
2022-11-24 09:15:01 +0100

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script>
+  export default{
+    data(){
+      return {
+        title: 'Titolo pagina'
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style scoped>
+h1{
+  color: black;
+}
+</style>
